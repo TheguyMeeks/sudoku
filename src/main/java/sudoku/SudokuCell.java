edd5105848bbb57288c0f9e65360d544049b21a0
Meeks <meekd2k@gmail.com>
2026-09-24 02:20:15 -0600

    feat (SudokuCell): create small wrapper class for javafx labels
    This class creates a cell type that owns and enforces its own locking rule instead of trusting callers to remember

---
+++ src/main/java/sudoku/SudokuCell.java
@@ -0,0 +1,30 @@
+package sudoku;
+
+import javafx.scene.control.Label;
+
+public class SudokuCell {
+    private Label label;
+    private boolean isLocked;
+
+    public SudokuCell(Label label) {
+        this.label = label;
+        isLocked = false;
+    }
+
+    // the only function that should call this is drawGame()
+    public Label getLabel() {
+        return this.label;
+    }
+
+    public void lock() {
+        isLocked = true;
+    }
+
+    public boolean trySetValue(String s) {
+        if (isLocked) {
+            return false;
+        }
+        this.label.setText(s);
+        return true;
+    }
+}
